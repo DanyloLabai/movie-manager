91396b2f21df277670d3955f17b1c824f262b283
DanyloLabai <danylolabai@gmail.com>
2026-04-28 21:23:09 +0300

    updated bscrypt

--- movie-backend/src/auth/auth.service.ts
+++ movie-backend/src/auth/auth.service.ts
@@ -7,7 +7,7 @@ import {
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import * as crypto from 'crypto';
 import { User } from '../users/users.entity';
 import { SignUpDto } from './dto/sign-up.dto';
